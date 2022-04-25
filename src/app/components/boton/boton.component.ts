2771937f1ac6c2c98c0d0842a03a63e65f6d5dee
morronepablo <morronepablo@gmail.com>
2022-04-25 14:32:01 -0300

    agregado de funcion para apuestas de mas de 6 numeros

--- src/app/components/boton/boton.component.ts
+++ src/app/components/boton/boton.component.ts
@@ -193,31 +193,157 @@ export class BotonComponent implements OnInit {
       "numero": "45"
     }
   ];
+
+  public jackArray = [
+    {
+      "id": 100,
+      "numero": "0"
+    },
+    {
+      "id": 101,
+      "numero": "1"
+    },
+    {
+      "id": 102,
+      "numero": "2"
+    },
+    {
+      "id": 103,
+      "numero": "3"
+    },
+    {
+      "id": 104,
+      "numero": "4"
+    },
+    {
+      "id": 105,
+      "numero": "5"
+    },
+    {
+      "id": 106,
+      "numero": "6"
+    },
+    {
+      "id": 107,
+      "numero": "7"
+    },
+    {
+      "id": 108,
+      "numero": "8"
+    },
+    {
+      "id": 109,
+      "numero": "9"
+    }
+  ];
+
   public apuestaUsuario = [];
 
+  public jackUsuario = 0;
+
   public contador = 0;
+
+  public contadorJack = 0;
+
+  public playStart = false;
+
+  public apuesta = this.goApuestaBase();
 
   constructor() {
 
   }
 
-
-
   ngOnInit(): void {
 
+  }
+
+  // Funcion poner ceros a la izquierda
+  zfill(number: number, width: number) {
+    var numberOutput = Math.abs(number); /* Valor absoluto del número */
+    var length = number.toString().length; /* Largo del número */
+    var zero = "0"; /* String de cero */
+
+    if (width <= length) {
+        if (number < 0) {
+             return ("-" + numberOutput.toString());
+        } else {
+             return numberOutput.toString();
+        }
+    } else {
+        if (number < 0) {
+            return ("-" + (zero.repeat(width - length)) + numberOutput.toString());
+        } else {
+            return ((zero.repeat(width - length)) + numberOutput.toString());
+        }
+    }
+  }
+
+  // Funcion numero aleatorio
+
+  randomNumber(min, max){
+    let r = Math.random()*(max-min) + min
+    return Math.floor(r)
+  }
+
+  // Funcion llamada a apuesta base
+
+  goApuestaBase() {
+    let apuestaBase = 130;
+    return apuestaBase;
+  }
+
+  // Switch para apuestas de 7 a 15 números
+  goSwitchApuesta(contador) {
+    switch (contador) {
+      case 7:
+          this.apuesta = this.goApuestaBase() * 7;
+        break;
+      case 8:
+          this.apuesta = this.goApuestaBase() * 28;
+        break;
+      case 9:
+          this.apuesta = this.goApuestaBase() * 84;
+        break;
+      case 10:
+          this.apuesta = this.goApuestaBase() * 210;
+        break;
+      case 11:
+          this.apuesta = this.goApuestaBase() * 462;
+        break;
+      case 12:
+          this.apuesta = this.goApuestaBase() * 924;
+        break;
+      case 13:
+          this.apuesta = this.goApuestaBase() * 1716;
+        break;
+      case 14:
+          this.apuesta = this.goApuestaBase() * 3003;
+        break;
+      case 15:
+          this.apuesta = this.goApuestaBase() * 5005;
+        break;
+      default:
+          this.apuesta = this.goApuestaBase();
+        break;
+    }
   }
 
   goBoton(id:any): void {
     let page = document.getElementById(id);
     let inactivo = page.classList.contains("botonActivo");
-    if(this.contador >= 0 && this.contador < 6 ) {
+    if(this.contador >= 0 && this.contador < 15 ) {
       console.log(id);
       console.log(page);
+      this.playStart= false;
+      console.log("playStart ", this.playStart);
+
       if(inactivo == false) {
         page.className = "botonActivo";
         this.contador = this.contador + 1;
         this.apuestaUsuario.push(id);
         console.log(this.apuestaUsuario);
+        console.log("APUESTA :", this.apuestaUsuario[0]);
+
       } else {
         page.className = "";
         this.contador = this.contador - 1;
@@ -227,9 +353,12 @@ export class BotonComponent implements OnInit {
         console.log(this.apuestaUsuario);
       }
       console.log("Contador ",this.contador);
-      console.log(inactivo);
     }
-    if(this.contador == 6) {
+    if(this.contador >= 6 && this.contadorJack == 1) {
+      this.playStart = true;
+      console.log("playStart ", this.playStart);
+    }
+    if(this.contador == 15) {
       if(inactivo == true) {
         page.className = "";
         this.contador = this.contador - 1;
@@ -237,19 +366,139 @@ export class BotonComponent implements OnInit {
         console.log("posicion: ",apuesta);
         this.apuestaUsuario.splice(apuesta,1)
         console.log(this.apuestaUsuario);
+        this.playStart= false;
       }
-      console.log("Contador 2",this.contador);
-      console.log(inactivo);
+      console.log("Contador 2",this.contadorJack);
+      if(this.contadorJack == 1 && this.contador > 5) {
+        this.playStart = true;
+        console.log("playStart ", this.playStart);
+      }
     }
-
-
+    this.goSwitchApuesta(this.contador);
+    console.log("Apuesta ----> ", this.apuesta);
     console.log("Contador ",this.contador);
+  }
 
+  goSuerte(): void {
+    console.log("Suerte --->");
+    console.log("apuestas ---> ", this.apuestaUsuario.length);
+    console.log("apuestas datos ---> ", this.apuestaUsuario);
+    if(this.apuestaUsuario.length > 0) {
+      for (let p = 0; p < this.apuestaUsuario.length; p++) {
+        let element = this.apuestaUsuario[p];
+        console.log(element);
+        console.log(p);
+        let page = document.getElementById(element);
+        page.className = "";
+        console.log(page);
+      }
+      this.contador = 0;
+      this.apuestaUsuario = [];
+      console.log("Aleatorio ----> ", this.randomNumber(0, 47));
+      for (let b = 0; b < 45; b++) {
+        let page = document.getElementById(`${b}`);
+        page.className = "";
+      }
+      for (let i = 0; i < 6; i++) {
+        let nAleatorio = this.randomNumber(0, 47);
+        if(this.apuestaUsuario.indexOf(nAleatorio) == -1) {
+          console.log("numero no existe");
+          this.apuestaUsuario.push(nAleatorio);
+          let element = this.apuestaUsuario[i];
+          let page = document.getElementById(element);
+          page.className = "botonActivo";
+          // this.contador = this.contador + 1;
+        } else {
+          console.log("numero existe");
+          i--;
+        }
+      }
+      console.log("apuestas datos ---> ", this.apuestaUsuario);
+      this.contador = 6;
+      console.log("Contador ",this.contador);
+    } else {
+      console.log("Aleatorio ----> ", this.randomNumber(0, 47));
+      for (let b = 0; b < 45; b++) {
+        let page = document.getElementById(`${b}`);
+        page.className = "";
+      }
+      for (let i = 0; i < 6; i++) {
+        let nAleatorio = this.randomNumber(0, 47);
+        if(this.apuestaUsuario.indexOf(nAleatorio) == -1) {
+          console.log("numero no existe");
+          this.apuestaUsuario.push(nAleatorio);
+          let element = this.apuestaUsuario[i];
+          let page = document.getElementById(element);
+          page.className = "botonActivo";
+          // this.contador = this.contador + 1;
+        } else {
+          console.log("numero existe");
+          i--;
+        }
+      }
+      console.log("apuestas datos ---> ", this.apuestaUsuario);
+      this.contador = 6;
+      console.log("Contador ",this.contador);
+    }
+    if(this.contador >= 6 && this.contadorJack == 1) {
+      this.playStart = true;
+      console.log("playStart ", this.playStart);
+    } else {
+      this.playStart = false;
+      console.log("playStart ", this.playStart);
+    }
+    this.goSwitchApuesta(this.contador);
+    console.log("Apuesta ----> ", this.apuesta);
+  }
+
+  goJack(id:any): void {
+    let jack = document.getElementById(id);
+    let inactivo = jack.classList.contains("botonActivo");
+    if(this.contadorJack >= 0 && this.contadorJack < 1 ) {
+      console.log(id);
+      console.log(jack);
+      this.playStart = false;
+      console.log("playStart ", this.playStart);
+      if(inactivo == false) {
+        jack.className = "botonActivo";
+        this.contadorJack = this.contadorJack + 1;
+        this.jackUsuario = id;
+        this.playStart = false;
+        console.log("jackPot :", this.jackUsuario);
+      } else {
+        jack.className = "";
+        this.contadorJack = this.contadorJack - 1
+        this.jackUsuario = null;
+        this.playStart = false;
+        console.log("jackPot :", this.jackUsuario);
+      }
+      console.log("Contador 2", this.contadorJack);
+    }
+    if(this.contadorJack == 1) {
+      if(inactivo == true) {
+        jack.className = "";
+        this.contadorJack = this.contadorJack - 1;
+        this.jackUsuario = null;
+        this.playStart = false;
+        console.log("jackPot :", this.jackUsuario);
+      } else {
+        this.playStart = false;
+      }
+      console.log("Contador 2", this.contadorJack);
+      if(this.contador >= 6 && this.contadorJack == 1) {
+        this.playStart = true;
+        console.log("playStart ", this.playStart);
+      } else {
+        this.playStart = false;
+        console.log("playStart ", this.playStart);
+      }
+    }
+    console.log("Contador ", this.contador);
   }
 
 
   goPlay(): void {
-
+    console.log("playStart: ", this.playStart);
   }
 
 }
